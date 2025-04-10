5307337d3ed428ee6e2592738a7193c05eebf5c0
김민지 <ghi512151@gmail.com>
2025-04-10 15:51:58 +0900

    [Feat] #2 - 로그인, 회원가입 관련 Dto 생성

---
+++ src/main/java/com/BioTy/Planty/dto/user/LoginRequestDto.java
@@ -0,0 +1,11 @@
+package com.BioTy.Planty.dto.user;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class LoginRequestDto {
+    private String email;
+    private String password;
+}
