817705edde0e4e3890a6f66df0b6ffe420181789
김민지 <ghi512151@gmail.com>
2025-04-15 14:21:21 +0900

    [feat] #3 - UserPlantSummaryResponseDto 생성

---
+++ src/main/java/com/BioTy/Planty/dto/userPlant/UserPlantSummaryResponseDto.java
@@ -0,0 +1,36 @@
+package com.BioTy.Planty.dto.userPlant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor
+public class UserPlantSummaryResponseDto {
+    private Long userPlantId;
+    private String nickname;
+    private String imageUrl;
+    private LocalDate adoptedAt;
+
+    private Status status;
+    private Personality personality;
+
+    @Getter
+    @AllArgsConstructor
+    public static class Status{
+        private Integer temperatureScore;
+        private Integer lightScore;
+        private Integer humidityScore;
+        private String message;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Personality{
+        private String label;
+        private String emoji;
+        private String color;
+    }
+
+}
