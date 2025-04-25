4294a9375596f0ace0c7a5250c22b1f730a1156a
김민지 <ghi512151@gmail.com>
2025-04-25 21:52:27 +0900

    [feat] #5 - SensorLogs, PlantEnvStandards 엔티티 생성

---
+++ src/main/java/com/BioTy/Planty/entity/SensorLogs.java
@@ -0,0 +1,28 @@
+package com.BioTy.Planty.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SensorLogs {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "device_id", nullable = false)
+    private IotDevice iotDevice;
+
+    private BigDecimal temperature;
+    private BigDecimal humidity;
+    private BigDecimal light;
+
+    private LocalDateTime recordedAt;
+}
