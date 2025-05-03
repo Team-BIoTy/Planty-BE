54e91c0c35994169bc212581d9edae8b6b3e4eb2
김민지 <ghi512151@gmail.com>
2025-05-03 21:43:06 +0900

    [feat] #6 - 메세지 전송 시 lastSentAt 갱신

--- src/main/java/com/BioTy/Planty/service/ChatService.java
+++ src/main/java/com/BioTy/Planty/service/ChatService.java
@@ -8,6 +8,7 @@ import com.BioTy.Planty.repository.ChatMessageRepository;
 import com.BioTy.Planty.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -15,6 +16,7 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ChatService {
     private final ChatRoomRepository chatRoomRepository;
     private final ChatMessageRepository chatMessageRepository;
@@ -42,15 +44,27 @@ public class ChatService {
 
     // 3. 메시지 전송
     public ChatMessageResponseDto sendMessage(Long chatRoomId, String message) {
-        // 사용자 메시지 저장
-        ChatMessage userMsg = chatMessageRepository.save(new ChatMessage(chatRoomId, ChatMessage.Sender.USER, message));
+        // 1) 사용자 메시지 저장
+        ChatMessage userMsg = chatMessageRepository.save(
+                new ChatMessage(chatRoomId, ChatMessage.Sender.USER, message)
+        );
 
-        // AI 서버와 통신해서 응답 받아오기 (TODO)
+        // 2) AI 서버와 통신해서 챗봇 응답 받기 & 저장 (TODO)
         String botReply = "테스트 응답입니다"; // 임시
+        ChatMessage botMsg = chatMessageRepository.save(
+                new ChatMessage(chatRoomId, ChatMessage.Sender.BOT, botReply)
+        );
 
-        // 챗봇 메시지 저장
-        ChatMessage botMsg = chatMessageRepository.save(new ChatMessage(chatRoomId, ChatMessage.Sender.BOT, botReply));
+        // 3) 채팅방의 lastSenetAt 갱신
+        ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId)
+                .orElseThrow(() -> new IllegalArgumentException("채팅방이 존재하지 않습니다."));
+        chatRoom.updateLastSentAt(LocalDateTime.now());
+        chatRoomRepository.save(chatRoom);
 
-        return new ChatMessageResponseDto(botMsg.getSender().name(), botMsg.getMessage(), botMsg.getTimestamp());
+        return new ChatMessageResponseDto(
+                botMsg.getSender().name(),
+                botMsg.getMessage(),
+                botMsg.getTimestamp()
+        );
     }
 }
