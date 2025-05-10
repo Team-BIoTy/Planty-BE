9f795cd2091067aceacfb33acb19a40530c26cb6
김민지 <ghi512151@gmail.com>
2025-05-10 18:18:07 +0900

    [feat] #7 - PlantStatus에 builder 추가

--- src/main/java/com/BioTy/Planty/entity/PlantStatus.java
+++ src/main/java/com/BioTy/Planty/entity/PlantStatus.java
@@ -1,14 +1,14 @@
 package com.BioTy.Planty.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PlantStatus {
     @Id
