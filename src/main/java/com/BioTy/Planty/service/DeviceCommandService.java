da39d16e2c77e8a7bd46c6c3032359eb96c32af9
김민지 <ghi512151@gmail.com>
2025-05-11 14:13:30 +0900

    [feat] #7 - IoT 명령 전송 시 command, action_log 저장 로직 구현

--- src/main/java/com/BioTy/Planty/service/DeviceCommandService.java
+++ src/main/java/com/BioTy/Planty/service/DeviceCommandService.java
@@ -1,20 +1,46 @@
 package com.BioTy.Planty.service;
 
+import com.BioTy.Planty.entity.DeviceActionLog;
+import com.BioTy.Planty.entity.DeviceCommand;
 import com.BioTy.Planty.entity.UserPlant;
+import com.BioTy.Planty.repository.DeviceActionLogRepository;
+import com.BioTy.Planty.repository.DeviceCommandRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class DeviceCommandService {
     private final IotService iotService;
+    private final DeviceCommandRepository commandRepository;
+    private final DeviceActionLogRepository actionLogRepository;
 
     public void sendActions(UserPlant userPlant, List<String> actionTypes) {
         for (String action : actionTypes) {
             try {
+                // 1. 명령 전송
                 iotService.sendAction(userPlant.getId(), userPlant.getUser().getId(), action);
+
+                // 2. 전송한 명령 기록 (device_command)
+                DeviceCommand command = DeviceCommand.builder()
+                        .iotDevice(userPlant.getIotDevice())
+                        .userPlant(userPlant)
+                        .commandType(action)
+                        .sentAt(LocalDateTime.now())
+                        .build();
+                commandRepository.save(command);
+
+                // 3. 실행 후 상태 기록 (device_action_log)
+                DeviceActionLog log = DeviceActionLog.builder()
+                        .command(command)
+                        .result("pending")
+                        .build();
+                actionLogRepository.save(log);
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
