21b72428445790d34b05119d9f7d03bb2a95378e
김민지 <ghi512151@gmail.com>
2025-05-20 03:08:33 +0900

    [feat] #8 - FCM 연동: firebase 초기화 설정 및 디펜던시 추가

---
+++ src/main/java/com/BioTy/Planty/config/FirebaseConfig.java
@@ -0,0 +1,37 @@
+package com.BioTy.Planty.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+@Configuration
+@Slf4j
+public class FirebaseConfig {
+
+    @Value("${fcm.firebase_config_path}")
+    private String firebaseConfigPath;
+
+    @PostConstruct
+    public void initialize() throws IOException {
+        try {
+            GoogleCredentials googleCredentials = GoogleCredentials
+                    .fromStream(new ClassPathResource(firebaseConfigPath).getInputStream());
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(googleCredentials)
+                    .build();
+            if(FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+                log.info("Firebase 앱이 초기화되었습니다.");
+            }
+        } catch (IOException e){
+            log.error(e.getMessage());
+        }
+    }
+}
