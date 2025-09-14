b8b68b025142a12355c016a0c9eb079d18c4f2b9
김민지 <ghi512151@gmail.com>
2025-09-14 22:32:55 +0900

    [fix] Timezone 고정 (asia/seoul)

--- src/main/java/com/BioTy/Planty/PlantyApplication.java
+++ src/main/java/com/BioTy/Planty/PlantyApplication.java
@@ -1,9 +1,12 @@
 package com.BioTy.Planty;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @EnableScheduling
 @EnableJpaAuditing
@@ -12,6 +15,12 @@ public class PlantyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlantyApplication.class, args);
+	}
+
+	@PostConstruct
+	public void started() {
+		// JVM 기본 시간대를 KST로 고정
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
